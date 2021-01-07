3bf03b4c559bc48a3ebe81c0108b5aa91b737646
Reeshul <reeshul@hotmail.com>
2021-01-07 18:04:28 +0000

    Updated version

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,10 +2,12 @@ require './lib/bike.rb'
 
 class DockingStation
   DEFAULT_CAPACITY = 20
+
   def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = Array.new
     @capacity = capacity
   end
+  attr_reader :capacity
   def release_bike
     raise "No bikes available!" if empty?
     return @bikes.pop  
@@ -14,7 +16,6 @@ class DockingStation
     raise "Bikes cannot be docked - station is full!" if full?
     return @bikes.push(bike)
   end
-  attr_reader :bike
   private
   def full?
     @bikes.count == @capacity
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,10 @@ require 'docking_station'
 describe DockingStation do
   it {should respond_to(:release_bike) }
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
   it "releases working bikes" do
     bike = Bike.new
     subject.dock(bike)
@@ -16,9 +20,15 @@ describe DockingStation do
     expect(subject.release_bike).to eq bike
   end
 
-  it "bikes cannot be docked as station is full" do
+  it "bikes cannot be docked as station is full at default capacity" do
     20.times { subject.dock(Bike.new) }
     expect { subject.dock(Bike.new) }.to raise_error("Bikes cannot be docked - station is full!")
+  end
+
+  it "bikes cannot be docked as station is full (1 space)" do
+    station = DockingStation.new(1)
+    station.dock(Bike.new)
+    expect { station.dock(Bike.new) }.to raise_error("Bikes cannot be docked - station is full!")
   end
 
   it "no bikes available" do
