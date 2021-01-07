83ca48b109625fccaf9986127f2fedb767c28b84
Reeshul <reeshul@hotmail.com>
2021-01-07 17:41:28 +0000

    Initialized default capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,9 @@ require './lib/bike.rb'
 
 class DockingStation
   DEFAULT_CAPACITY = 20
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = Array.new
+    @capacity = capacity
   end
   def release_bike
     raise "No bikes available!" if empty?
@@ -16,7 +17,7 @@ class DockingStation
   attr_reader :bike
   private
   def full?
-    @bikes.count == DEFAULT_CAPACITY
+    @bikes.count == @capacity
   end
   def empty?
     @bikes.empty?
