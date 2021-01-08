d16aa889c98409ee504fbb9587f19dc50105bea1
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-08 15:59:34 +0000

    added broken attribute to bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,11 @@
 class Bike
+
+  attr_accessor :broken
+
+  def initialize
+    @broken = false
+  end
+
   def working?
     true
   end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,8 @@ require 'bike'
 
 describe Bike do
   it { should respond_to(:working?) }
+
+  it "has a broken attribute" do
+    expect(subject).to respond_to :broken
+  end
 end
