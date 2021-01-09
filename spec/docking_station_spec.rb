ef2b86613e297abc6178a0f2b0ab872f9684d86d
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-09 10:24:58 +0000

    refactored to remove repetition in doubles creation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require 'docking_station'
 # In spec/docking_station_spec.rb
 describe DockingStation do
 
-  let(:bike) { double :bike }
+  let(:bike) { double :bike, broken: false, :broken= => false }
 
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
@@ -14,15 +14,11 @@ describe DockingStation do
     it {should respond_to(:release_bike) }
 
     it "returns docked bikes" do
-      allow(bike).to receive(:broken).and_return(false)
-      allow(bike).to receive(:broken=).and_return(false)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
     
     it "release working bike even if stored first" do
-      allow(bike).to receive(:broken).and_return(false)
-      allow(bike).to receive(:broken=).and_return(false)
       bike1 = bike
       bike2 = bike
       subject.dock(bike1)
@@ -31,8 +27,7 @@ describe DockingStation do
     end
 
     it "doesn't release broken bike" do
-      allow(bike).to receive(:broken).and_return(true)
-      allow(bike).to receive(:broken=).and_return(true)
+      bike = double(:bike, broken: true, :broken= => true)
       subject.dock(bike, true)
       expect { subject.release_bike }.to raise_error "No working bikes available"
     end
@@ -43,8 +38,6 @@ describe DockingStation do
     end
 
     it "releases working bikes" do
-      allow(bike).to receive(:broken).and_return(false)
-      allow(bike).to receive(:broken=).and_return(false)
       subject.dock(bike)
       expect(subject.release_bike.broken).to eq false
     end
@@ -54,9 +47,6 @@ describe DockingStation do
   describe "#dock" do
 
     it "docks something" do
-      allow(bike).to receive(:broken).and_return(false)
-      allow(bike).to receive(:broken=).and_return(false)
-
       subject.dock(bike)
       expect(subject.bikes.count).to eq 1
     end
@@ -67,17 +57,12 @@ describe DockingStation do
 
 
     it "bikes cannot be docked as station is full at default capacity" do
-      allow(bike).to receive(:broken).and_return(false)
-      allow(bike).to receive(:broken=).and_return(false)
-      
       DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) }
       expect { subject.dock(bike) }.to raise_error("Bikes cannot be docked - station is full!")
     end
 
     it "able to report broken bikes" do
-      allow(bike).to receive(:broken).and_return(true)
-      allow(bike).to receive(:broken=).and_return(true)
-      
+      bike = double(:bike, broken: true, :broken= => true)
       subject.dock(bike, true)
       expect(bike.broken).to eq true
     end
