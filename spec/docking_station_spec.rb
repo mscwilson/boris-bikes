bb797f2af9f971b9230bfb0223ad8e098bb0ba4f
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-09 10:13:41 +0000

    Changed tests to use doubles (not DRY at all though)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,8 @@ require 'docking_station'
 # In spec/docking_station_spec.rb
 describe DockingStation do
 
+  let(:bike) { double :bike }
+
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
@@ -11,45 +13,51 @@ describe DockingStation do
 
     it {should respond_to(:release_bike) }
 
+    it "returns docked bikes" do
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:broken=).and_return(false)
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    
     it "release working bike even if stored first" do
-      bike1 = Bike.new
-      bike2 = Bike.new
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:broken=).and_return(false)
+      bike1 = bike
+      bike2 = bike
       subject.dock(bike1)
       subject.dock(bike2, true)
       expect(subject.release_bike).to eq bike1
     end
 
     it "doesn't release broken bike" do
-      bike = Bike.new
+      allow(bike).to receive(:broken).and_return(true)
+      allow(bike).to receive(:broken=).and_return(true)
       subject.dock(bike, true)
       expect { subject.release_bike }.to raise_error "No working bikes available"
     end
 
-    it "returns docked bikes" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
 
     it "no bikes available" do
       expect { subject.release_bike }.to raise_error("No bikes available!")
     end
 
     it "releases working bikes" do
-      bike = Bike.new
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:broken=).and_return(false)
       subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      expect(subject.release_bike.broken).to eq false
     end
-
-
-
   end
 
 
   describe "#dock" do
 
     it "docks something" do
-      subject.dock(Bike.new)
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:broken=).and_return(false)
+
+      subject.dock(bike)
       expect(subject.bikes.count).to eq 1
     end
 
@@ -59,18 +67,17 @@ describe DockingStation do
 
 
     it "bikes cannot be docked as station is full at default capacity" do
-      20.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error("Bikes cannot be docked - station is full!")
-    end
-
-    it "bikes cannot be docked as station is full (1 space)" do
-      station = DockingStation.new(1)
-      station.dock(Bike.new)
-      expect { station.dock(Bike.new) }.to raise_error("Bikes cannot be docked - station is full!")
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:broken=).and_return(false)
+      
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error("Bikes cannot be docked - station is full!")
     end
 
     it "able to report broken bikes" do
-      bike = Bike.new
+      allow(bike).to receive(:broken).and_return(true)
+      allow(bike).to receive(:broken=).and_return(true)
+      
       subject.dock(bike, true)
       expect(bike.broken).to eq true
     end
